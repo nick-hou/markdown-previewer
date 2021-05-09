6b82eb921dc69225698abe549a8c904013b8595b
Nick Hou <hounicholas1@gmail.com>
2021-05-09 18:08:59 -0500

    reverted parsing logic to markedJS

--- src/convertText.js
+++ src/convertText.js
@@ -1,6 +1,10 @@
 // Returns a string containing HTML tags. String is then parsed to React HTML in the Previewer component.
 
-// Option to use the markedJS library to convert text to markdown. Mostly for testing purposes
+/*
+This program uses markedJS to reformat text as markdown.
+I've made my own function below that uses regex to do the same thing. It gets most (but not all) syntax.
+*/
+
 import marked from 'marked'
 
 const convertText = (input) => {
@@ -18,25 +22,32 @@ const convertText = (input) => {
   //   .replace(/^## (.*$)/gim, '<h2>$1</h2>')
   //   .replace(/^# (.*$)/gim, '<h1>$1</h1>')
   //   // blockquote >
-  //   .replace(/^\> (.*$)/gim, '<blockquote>$1</blockquote>')
-  //   // code `
-  //   .replace(/\`(.*$)\`/gi, '<code>$1</code>')
+  //   .replace(/^> (.*$)/gim, '<blockquote>$1</blockquote>')
+  //   // multi line code ```
+  //   .replace(/```([^\r]*?)```/gim, '<code>$1</code>')
+  //   // single line code `
+  //   .replace(/`(.*?)`/gim, '<code>$1</code>')
   //   // bold and italic ***, ___
   //   .replace(/\*\*\*(.*)\*\*\*/gim, '<b><i>$1</i></b>')
-  //   .replace(/\_\_\_(.*)\_\_\_/gim, '<b><i>$1</i></b>')
+  //   .replace(/___(.*)___/gim, '<b><i>$1</i></b>')
   //   //bold **, __
   //   .replace(/\*\*(.*)\*\*/gim, '<b>$1</b>')
-  //   .replace(/\_\_(.*)\_\_/gim, '<b>$1</b>')
+  //   .replace(/__(.*)__/gim, '<b>$1</b>')
   //   // italic *, _
   //   .replace(/\*(.*)\*/gim, '<i>$1</i>')
-  //   .replace(/\_(.*)\_/gim, '<i>$1</i>')
+  //   .replace(/_(.*)_/gim, '<i>$1</i>')
+  //   // strikethrough ~~
+  //   .replace(/~~(.*)~~/gim, '<s>$1</s>')
   //   // img [caption](link)
 	// 	.replace(/!\[(.*?)\]\((.*?)\)/gim, "<img alt='$1' src='$2' />")
   //   // hyperlink [text](link)
 	// 	.replace(/\[(.*?)\]\((.*?)\)/gim, "<a href='$2'>$1</a>")
-  //   // list items * + - #
-  //   .replace(/^([\d\+\#\*])+$ (.*$)/gim, '<li>$1</li>')
-  //   .replace(/^[-]+$ (.*$)/gim, '<li>$1</li>')
+  //   // list items * + -
+  //   .replace(/^[-*+]+ (.*$)/gim, '<li>$1</li>')
+  //   // list items, number
+  //   .replace(/^\d+. (.*$)/gim, '<li>$1</li>')
+  //   // horizontal rules
+  //   .replace(/---/, '<hr />')
   //   // line breaks
   //   .replaceAll('\n', '<br />')
   //   .trim()
--- src/defaultInput.js
+++ src/defaultInput.js
@@ -1,10 +1,10 @@
 const defaultInput =
 `
 # This is my markdown previewer, built in React.
-
+---
 ## Type some markdown text in the editor, and watch it get stylized in the previewer in real time.
 
-### For tips on what you can do in markdown, check out [this cheatsheet](https://www.markdownguide.org/cheat-sheet/)
+### For a reminder on what you can do in markdown, check out [this cheatsheet](https://www.markdownguide.org/cheat-sheet/)
 
 \`function sayHi() {console.log('Hi')}\`
 
@@ -16,12 +16,14 @@ function sayHello = () => {
 }
 \`\`\`
 
-- One potato
-- Two potato!
++ One
+- two
+* three
+1. four
 
-> Here's a block quote
-
-This site uses [marked](https://www.npmjs.com/package/marked) to format markdown text, and [HTML react parser](https://www.npmjs.com/package/html-react-parser) to parse that into React elements.
+> The only way to deal with an unfree world is to become so absolutely free that your very existence is an act of rebellion.
+>
+> Albert Camus
 
 [Built by **Nick Hou**](http://www.nicholashou.com)
 
